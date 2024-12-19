6939e7e8e252bacd05223620672e0b69b8c4f54b
qds013 <qds013@yandex.ru>
2024-12-19 12:18:32 +0300

    2024-12-19

--- main.py
+++ main.py
@@ -35,22 +35,52 @@ def bot_send_help(message):
 
     Пример номера заказа: ORDER123
 
+    !!! Сейчас работает тестовый режим: заказ должен быть ORDER123
+
     При возникновении проблем обратитесь к менеджеру.
     """
     bot.reply_to(message, message_text)
 
+#handling any input as order number
 @bot.message_handler(func = lambda message: True)
 def bot_check_order(message):
     order_number = message.text.strip().upper()
-    logging.info(f"Получен запрос на заказ {order_number} от {message.from_user.username}")
-    bot.reply_to(message, "Запрос статуса заказа " + order_number)
+    logger.info(f"Получен запрос информации о заказе: {order_number} от: {message.from_user.username}")
+    # bot.reply_to(message, "Запрос статуса заказа " + order_number)
 
+    if not validate_order_number(order_number):
+        logger.info(f"Некорректный номер заказа {order_number}")
+        bot.reply_to(message, "🔴 Некорректный формат номера заказа.")
+        return
+    
+    try:
+        labels = ["Тестирование"]
+        logger.info(f"Заказ {order_number} статус {', ' . join(labels)}")
+        response = f"📦 Заказ №{order_number}\n"
+        response += f"📋 Статус: {', ' . join(labels)}\n"
+        bot.reply_to(message, response)    
+    except Exception as e:
+        logger.error(f"Ошибка при обработке заказа {order_number}: {str(e)}")
+        response = f"🔴 Произошла ошибка при поиске заказа №{order_number} . Попробуйте позже.\n\n"
+        bot.reply_to(message, response)
 
+# handling any incoming text by echo message
+"""
 @bot.message_handler(content_types=["text"])
 def echo(message):
     bot.send_message(message.chat.id, message.text)
     print("*** Message arrived: " + message.text)
+"""
 
-print("Telegram bot started...")
-bot.polling(none_stop=True)
-print("Telegram bot exited.")
+def validate_order_number(order_number):
+    return(order_number == "ORDER123")
+
+# запуск бота
+if __name__ == "__main__":
+    try:
+        logger.info("Telegram bot started...")
+        bot.polling(non_stop=True)
+    except Exception as e:
+        logger.error(f"Критическая ошибка: {str(e)}")
+    finally:
+        logger.info("Telegram bot exited.")
